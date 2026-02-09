dc73e43af446f2c2dbe9ac734724fb274716d116
bahaj <otaibm0p@gmail.com>
2026-02-09 10:34:27 +0100

    Initial Commit

---
+++ tomocustomerapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tomocustomerapp
+//
+//  Created by user294169 on 2/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tomocustomerapp/tomocustomerappApp.swift
@@ -0,0 +1,17 @@
+//
+//  tomocustomerappApp.swift
+//  tomocustomerapp
+//
+//  Created by user294169 on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct tomocustomerappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
